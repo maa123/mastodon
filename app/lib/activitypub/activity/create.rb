dc6ef1fc8fdaba6801677223f7ffb293070d07b1
maa123 <maa123.lq@gmail.com>
2024-03-19 13:03:16 +0900

    Update create.rb

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -439,7 +439,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     url = ENV.fetch('SPAM_CHECK_ENDPOINT') { 'false' }
     return (!@status.account.local? && @status.account.followers_count.zero? && @status.account.created_at > 1.day.ago && @mentions.count >= 2) if url == 'false'
     return false if @status.account.local?
-    return HTTP.post(url, json: { status: @status, account: @status.account, mentions: @mentions.map(&:account).map(&:username) }).code != '200'
+    HTTP.post(url, json: { status: @status, account: @status.account, mentions: @mentions.map(&:account).map(&:username) }).code != '200'
   rescue => e
     Rails.logger.warn "Error checking for spam: #{e}"
     false
