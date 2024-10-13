8a9e1708a036ab0c34ce839e2e0a2fc9984b99b7
maa123 <maa123.lq@gmail.com>
2024-10-13 12:43:10 +0900

    Update create.rb

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -442,7 +442,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     if (!@status.account.local? && @status.account.followers_count.zero? && @status.account.created_at > 1.day.ago && @mentions.count >= 2)
       return true if url == 'false'
       spam_response = HTTP.post("#{url}/check", json: { status: @status, account: @status.account, mentions: @mentions.map(&:account).map(&:username) })
-      HTTP.post("#{url}/log", json: { status: @status, account: @status.account, mentions: @mentions.map(&:account).map(&:username) })
+      HTTP.post("#{url}/log", json: { res: spam_response })
       Rails.logger.warn "Spam check response: #{spam_response}"
       return true
     else
